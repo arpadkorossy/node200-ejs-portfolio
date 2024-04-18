f6d9efdd6c4b6dc3c616d6e63a933d0852037466
arpadkorossy <korossy.arpad@gmail.com>
2024-04-18 14:54:15 -0400

    Minor updates

--- server.js
+++ server.js
@@ -178,5 +178,5 @@ app.get('/sheets-auth', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`listening at http://localhost:${PORT}`)
+    console.log(`Listening at http://localhost:${PORT}`)
 })
